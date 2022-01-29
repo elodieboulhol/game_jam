ad1a48619ebf710560070a6a2b5ee9f21712aee8
Simsim <none@free.fr>
2022-01-29 18:12:18 +0200

    add chest sound

--- core/src/com/mygdx/gamejam/model/Chest.java
+++ core/src/com/mygdx/gamejam/model/Chest.java
@@ -1,8 +1,13 @@
 package com.mygdx.gamejam.model;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class Chest extends GameObject {
 	
 	private boolean isOpen = false;
+	
+	private static Sound chestSound = Gdx.audio.newSound(Gdx.files.internal("sound/chest_effect.mp3"));
 	
 	public Chest(Coordinates coord, TileMap map) {
 		super(coord, map);
@@ -14,5 +19,6 @@ public class Chest extends GameObject {
 
 	public void setOpen(boolean isOpen) {
 		this.isOpen = isOpen;
+		chestSound.play();
 	}
 }
